e08b78c427133dd05db999c08905054a001d0cc6
Lee Waggoner <lwaggoner@roadrunner.com>
2023-10-10 13:50:05 +0800

    Updated gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,6 @@ plugins {
 
 val cocktailDBApiKey: String = gradleLocalProperties(rootDir).getProperty("COCKTAIL_DB_API_KEY")
 
-
 android {
     namespace = "com.wreckingballsoftware.fiveoclocksomewhere"
     compileSdk = 34
@@ -66,14 +65,14 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.activity:activity-compose:1.8.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.core:core-splashscreen:1.0.1")
-    implementation("androidx.navigation:navigation-compose:2.7.2")
+    implementation("androidx.navigation:navigation-compose:2.7.4")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
     implementation("androidx.room:room-runtime:2.5.2")
     annotationProcessor("androidx.room:room-compiler:2.5.2")
