d70243833191f93c0fb973e92ef8da909f01256c
Lee Waggoner <lwaggoner@roadrunner.com>
2023-10-30 12:01:48 -0700

    Added CocktailDB api key

--- app/src/main/java/com/wreckingballsoftware/fiveoclocksomewhere/network/CocktailDBService.kt
+++ app/src/main/java/com/wreckingballsoftware/fiveoclocksomewhere/network/CocktailDBService.kt
@@ -6,15 +6,15 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface CocktailDBService {
-    @GET("json/v1/${BuildConfig.COCKTAILDB_AUTH_KEY}/search.php")
+    @GET("json/v2/${BuildConfig.COCKTAILDB_AUTH_KEY}/search.php")
     suspend fun getCocktail(
         @Query("s") cocktailName: String,
     ): ApiCocktails
 
-    @GET("json/v1/${BuildConfig.COCKTAILDB_AUTH_KEY}/random.php")
+    @GET("json/v2/${BuildConfig.COCKTAILDB_AUTH_KEY}/random.php")
     suspend fun getRandomCocktail(): ApiCocktails
 
-    @GET("json/v1/${BuildConfig.COCKTAILDB_AUTH_KEY}/lookup.php")
+    @GET("json/v2/${BuildConfig.COCKTAILDB_AUTH_KEY}/lookup.php")
     suspend fun getCocktailById(
         @Query("i") cocktailId: String
     ): ApiCocktails
