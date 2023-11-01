93e4bb8c697a0110abebabe3ce95f3b5f6a7af6a
Lee Waggoner <lwaggoner@roadrunner.com>
2023-11-01 11:34:25 -0700

    Play store fix.

--- app/src/main/java/com/wreckingballsoftware/fiveoclocksomewhere/ui/displaycocktail/models/DisplayCocktailState.kt
+++ app/src/main/java/com/wreckingballsoftware/fiveoclocksomewhere/ui/displaycocktail/models/DisplayCocktailState.kt
@@ -1,5 +1,9 @@
 package com.wreckingballsoftware.fiveoclocksomewhere.ui.displaycocktail.models
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class DisplayCocktailState(
     val isLoading: Boolean = false,
     val cocktailName: String = "",
@@ -8,4 +12,4 @@ data class DisplayCocktailState(
     val cocktailIngredients: List<String> = listOf(),
     val cocktailMeasures: List<String> = listOf(),
     val cocktailInstructions: String = "",
-)
+) : Parcelable
