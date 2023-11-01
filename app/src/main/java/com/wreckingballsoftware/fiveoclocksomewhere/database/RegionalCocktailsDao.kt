3b03b97b41bf3ba8f5721ff226b9ee8b6af36020
Lee Waggoner <lwaggoner@roadrunner.com>
2023-10-31 18:50:32 -0700

    Fixed query and added keystore to gitignore

--- app/src/main/java/com/wreckingballsoftware/fiveoclocksomewhere/database/RegionalCocktailsDao.kt
+++ app/src/main/java/com/wreckingballsoftware/fiveoclocksomewhere/database/RegionalCocktailsDao.kt
@@ -5,7 +5,7 @@ import androidx.room.Query
 
 @Dao
 interface RegionalCocktailsDao {
-    @Query("SELECT * FROM cocktails " +
+    @Query("SELECT cocktails.id,cocktails.search_string FROM cocktails " +
             "INNER JOIN regional_cocktails ON cocktails.id=regional_cocktails.cocktail_id " +
             "WHERE regional_cocktails.place_id=:place")
     fun getAllCocktailsInPlace(place: Int): List<DBCocktails>
